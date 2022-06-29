0c5217167494decdfd23feae7bedffdbdd52b35a
Mustapha Essalih <messalih@e3r7p12.1337.ma>
2022-06-29 14:30:43 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// oop is the convert real life into program
+
+// by default the attributs in a class is private not be access in outside the class
+
+//  ,
+
+class Rectangle{
+
+    int age;
+    double salary;
+    string name;
+
+};
+
+
+int main()
+{
+
+    Rectangle r1;
+
+
+}
